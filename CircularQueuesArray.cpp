03a4dab443037f91aecf5775a13d47bc9ebe44b3
Cla1reee <h.harimurti.ft23@mail.umy.ac.id>
2024-05-30 15:01:02 +0700

    perbaikan1

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -1,32 +1,45 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
+//CircularQueuesArray_4NIM
+//15 commit
+
 class Queues {
-    private:
-        static const int max = 3;
-        int FRONT, REAR;
-        int queue_array[max];
+private:
+    static const int max = 3;
+    int FRONT, REAR;
+    int queue_array[max];
 
-    public:
-        Queues() {
-            FRONT = -1;
-            REAR = -1;
+public:
+    Queues() {
+        FRONT = -1;
+        REAR = -1;
+    }
+
+    void insert() {
+        int num;
+        cout << "Enter a numbah: ";
+        cin >> num;
+        cout << endl;
+
+        //cek apakah antrian penuh
+        if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
+            cout << "\nQueue overflow\n";
+            return ;
         }
 
-        void insert(){
-            int num;
-            cout << "Enter a number: ";
-            cin >> num;
-            cout << endl;
-
-            //Cek apakah antrian kosong 
-            if (FRONT == max - 1) {
-                FRONT = 0;
+        //cek apakah antrian kosong
+        if (FRONT == -1){
+            FRONT = 0;
+            REAR = 0;
+        }
+        else {
+            //jika rear berada di posisi terakhir array, kembali ke awal array
+            if (REAR == max - 1)
                 REAR = 0;
-            }
-            else {
-                REAR == REAR + 1;
-            }
-            queue_array[REAR] = num;
+            else
+                REAR = REAR + 1;
         }
+        queue_array[REAR] = num;
+    }
 };
