f10d2eb523060c2b4c7c02dacb6aa5a18004d4cc
Cla1reee <h.harimurti.ft23@mail.umy.ac.id>
2026-05-19 14:40:27 +0700

    Add queue operation options for remove and display in main function

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -103,6 +103,12 @@ int main() {
                 case '1':
                     q.insert();
                     break;
+                case '2':
+                    q.remove();
+                    break;
+                case '3':
+                    q.display();
+                    break;
                 case '4':
                     return 0;
                 default:
